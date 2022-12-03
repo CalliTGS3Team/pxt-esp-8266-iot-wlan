76fadc63b7cf31ec3b7d7e07a0b41e9fb683a5d8
Raik Andritschke <58722460+CalliTGS3@users.noreply.github.com>
2022-12-03 11:46:33 +0100

    New Adafruit.IO

--- esp8266.ts
+++ esp8266.ts
@@ -1,45 +1,132 @@
+/**
+ * ESP8266 ESP-01S extension for calliope.
+ * Serial interface.
+ *
+ * Fork from https://github.com/MKleinSB/pxt-esp-thingspeak 
+ * 
+ * @author Raik Andritschke
+ *
+ * Works with AZDelivery ESP8266 ESP-01S
+ * Tested: Adafruit.IO and ThingSpeak 
+ */
+
 //% weight=2 color=#1174EE icon="\uf1eb" block="ESP8266"
 
+enum ESP8266_LOGGING {
+    //% block="OFF"
+    //% block.loc.de="AUS"
+    LOGGING_OFF,
+    //% block="ON"
+    //% block.loc.de="EIN"
+    LOGGING_ON
+}
+
 namespace esp8266 {
-  let isWifiConnected = false;
+  
+    let isWifiConnected = false;
+    let TX = SerialPin.C17;
+    let RX = SerialPin.C16;
+    let RATE = BaudRate.BaudRate115200;
+    let LOGGING = ESP8266_LOGGING.LOGGING_OFF;
+
+    function logUSB(prefix: string, message: string): void {
+        if (LOGGING == ESP8266_LOGGING.LOGGING_ON) {
+            serial.redirectToUSB();
+            serial.writeLine(prefix + " " + message);
+            serial.redirect(TX, RX, RATE);
+        }
+    }
+
     /**
      * Setup Uart WiFi to connect to Wi-Fi
      */
-    //% block="Setup Wifi|TX %txPin|RX %rxPin|Baud rate %baudrate|SSID = %ssid|Password = %passwd"
+    //% block="Setup Wifi|TX %txPin|RX %rxPin|Baud rate %baudrate|SSID = %ssid|Password = %passwd|Logging = %logging"
+    //% block.loc.de="Mit dem WiFi Netzwerk verbinden|TX %txPin|RX %rxPin|Baud rate %baudrate|SSID = %ssid|Passwort = %passwd|Logging über USB = %logging"
     //% txPin.defl=SerialPin.C17
     //% rxPin.defl=SerialPin.C16
-    //% baudRate.defl=BaudRate.BaudRate115200
-    export function setupWifi(txPin: SerialPin, rxPin: SerialPin, baudRate: BaudRate, ssid: string, passwd: string) {
+    //% baudRate.defl=BaudRate.BaudRate9600
+    //% logging.defl=false
+    export function setupWifi(txPin: SerialPin, rxPin: SerialPin, baudRate: BaudRate, ssid: string, passwd: string, logging:ESP8266_LOGGING) {
         let result = 0
+        TX = txPin
+        RX = rxPin
+        RATE = baudRate
+        LOGGING = logging
 
         isWifiConnected = false
-
         serial.redirect(
-            txPin,
-            rxPin,
-            baudRate
+            TX,
+            RX,
+            RATE
         )
+        serial.setTxBufferSize(128)
+        serial.setRxBufferSize(128)
 
         sendAtCmd("AT")
+        result = waitAtResponse("OK", "ERROR", "None", 1000)
+
+        sendAtCmd("AT+GMR")
         result = waitAtResponse("OK", "ERROR", "None", 1000)
 
         sendAtCmd("AT+CWMODE=1")
         result = waitAtResponse("OK", "ERROR", "None", 1000)
 
         sendAtCmd(`AT+CWJAP="${ssid}","${passwd}"`)
-        result = waitAtResponse("WIFI GOT IP", "ERROR", "None", 20000)
+        result = waitAtResponse("WIFI GOT IP", "ERROR", "None", 5000)
 
         if (result == 1) {
             isWifiConnected = true
+
         }
     }
 
+    function waitAtResponse(target1: string, target2: string, target3: string, timeout: number) {
+        let buffer = ""
+        let start = input.runningTime()
+
+        while ((input.runningTime() - start) < timeout) {
+            buffer += serial.readString()
+        }    
+            if (buffer.includes(target1)) { 
+                logUSB('Response1',buffer)
+                return 1
+            }
+            if (buffer.includes(target2)) {
+                logUSB('Response2',buffer)
+                return 2
+            }
+            if (buffer.includes(target3)) {
+                logUSB('Response3',buffer)
+                return 3
+            }
+//        }
+
+        logUSB('Response0',buffer)
+        return 0
+    }
+
+    function sendAtCmd(cmd: string) {
+        serial.writeString(cmd + "\u000D\u000A")
+    }
+
     /**
-     * Check if Grove - Uart WiFi V2 is connected to Wifi
+     * Check if WiFi is connected
      */
     //% block="Wifi OK?"
     export function wifiOK() {
         return isWifiConnected
+    }
+
+    /**
+     * Disconnect from the wifi network.
+     */
+    //% block="Disconnect from wifi network"
+    //% block.loc.de="Vom WiFi Netzwerk trennen"
+    export function disconnect(): void {
+        if (isWifiConnected) {
+            sendAtCmd("AT+CWQAP")
+            waitAtResponse("OK", "ERROR", "None", 2000)
+        }
     }
 
     /**
@@ -76,9 +163,12 @@ namespace esp8266 {
             if (!isNaN(field7)) data = data + "&field7=" + field7
             if (!isNaN(field8)) data = data + "&field8=" + field8
 
+            logUSB('Send data',data)
+
             sendAtCmd("AT+CIPSEND=" + (data.length + 2))
-            result = waitAtResponse(">", "OK", "ERROR", 2000)
-            if (result == 3) continue
+            // result = waitAtResponse(">", "OK", "ERROR", 2000)
+            // if (result == 3) continue
+            basic.pause(500)
             sendAtCmd(data)
             result = waitAtResponse("SEND OK", "SEND FAIL", "ERROR", 5000)
 
@@ -86,6 +176,71 @@ namespace esp8266 {
             // sendAtCmd("AT+CIPCLOSE")
             // waitAtResponse("OK", "ERROR", "None", 2000)
 
+            if (result == 1) break
+        }
+    }
+
+    /**
+     * Send data to AdafruitIO
+     */
+    //% block="Send Data to your AdafruitIO Channel|Username %username|AdafruitIO Key %apiKey|Feed %feed|Value %value"
+    //% block.loc.de="Sende Daten an deinen AdafruitIO Kanal|Benutzername %username|AdafruitIO Key %apiKey|Feed 1 %feed|Wert %value"
+    //% expandableArgumentMode="enabled"
+    //% adafruitIOKey.defl="your AdafruitIO Key"
+    export function sendToAdafruitIO(username: string, adafruitIOKey: string, feed: string, value: string) {
+        let result = 0
+        let retry = 2
+
+        // close the previous TCP connection
+        if (isWifiConnected) {
+            sendAtCmd("AT+CIPCLOSE")
+            waitAtResponse("OK", "ERROR", "None", 2000)
+        }
+
+        let body = "value=" + value
+        let newline = "\u000D\u000A"
+
+        while (isWifiConnected && retry > 0) {
+            retry = retry - 1;
+            // establish TCP connection
+            logUSB('Establish TCP',"")
+            sendAtCmd("AT+CIPSTART=\"TCP\",\"io.adafruit.com\",80,1")
+            result = waitAtResponse("OK", "ALREADY CONNECTED", "ERROR", 2000)
+            if (result == 3) continue
+
+            let data = "POST /api/v2/"+username+"/feeds/"+ feed +"/data"
+            data = data + " HTTP/1.1"
+            data = data + newline
+            data = data + "Host: io.adafruit.com"
+            data = data + newline
+            data = data + "User-Agent: curl/7.83.1"
+            data = data + newline
+            data = data + "X-AIO-Key: " + adafruitIOKey
+            data = data + newline
+            data = data + "Content-Length: " + body.length.toString()
+            data = data + newline
+            data = data + "Content-Type: application/x-www-form-urlencoded"
+            data = data + newline
+            data = data + "Accept: */*"
+            data = data + newline
+            data = data + newline
+            data = data + body
+
+            logUSB('Send data',data)
+
+            sendAtCmd("AT+CIPSEND=" + (data.length + 2).toString())
+            // result = waitAtResponse(">", "OK", "ERROR", 2000)
+            // if (result == 3) continue
+            basic.pause(500)
+            sendAtCmd(data)
+            result = waitAtResponse("SEND OK", "SEND FAIL", "ERROR", 5000)
+            if (result == 3) continue
+            
+            // close the TCP connection
+            logUSB('Close TCP',"")
+            sendAtCmd("AT+CIPCLOSE")
+            waitAtResponse("OK", "ERROR", "None", 2000)
+            
             if (result == 1) break
         }
     }
@@ -130,9 +285,12 @@ namespace esp8266 {
             data = data + "Accept: */*"
             data = data + "\u000D\u000A"
 
+            logUSB('Send data',data)
+
             sendAtCmd("AT+CIPSEND=" + (data.length + 2))
-            result = waitAtResponse(">", "OK", "ERROR", 2000)
-            if (result == 3) continue
+            // result = waitAtResponse(">", "OK", "ERROR", 2000)
+            // if (result == 3) continue
+            basic.pause(500)
             sendAtCmd(data)
             result = waitAtResponse("SEND OK", "SEND FAIL", "ERROR", 5000)
             // close the TCP connection
@@ -142,24 +300,5 @@ namespace esp8266 {
         }
     }
 
-    function waitAtResponse(target1: string, target2: string, target3: string, timeout: number) {
-        let buffer = ""
-        let start = input.runningTime()
-
-        while ((input.runningTime() - start) < timeout) {
-            buffer += serial.readString()
-
-            if (buffer.includes(target1)) return 1
-            if (buffer.includes(target2)) return 2
-            if (buffer.includes(target3)) return 3
-
-            basic.pause(100)
-        }
-
-        return 0
-    }
-
-    function sendAtCmd(cmd: string) {
-        serial.writeString(cmd + "\u000D\u000A")
-    }
 }
+
